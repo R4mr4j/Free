86724c68abfe72811f95cdb2e0f0e1cd33ada288
R9mr9j kum9w9t <132282378+R4mr4j@users.noreply.github.com>
2023-12-31 13:50:37 +0530

    Update free.py

--- free.py
+++ free.py
@@ -1,1 +1,30 @@
-import sex_MAHADI_ENC
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from sex_MAHADI_ENC import menu
+ 
+        menu()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from sex_MAHADI_ENC import menu
+ 
+ 
+        menu()
